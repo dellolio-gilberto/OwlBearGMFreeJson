8ad0d0ae24d992eecb59872aa771f1d9f5f3cac9
GilbertoIS <gilberto25112001@gmail.com>
2025-05-17 17:13:34 +0200

    API added

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,1 @@
-export const TTRPG_URL = "https://api.tabletop-almanac.com/api/v1/e5/statblock/search/?search_string=""&take=50&skip=0"
-//import.meta.env.VITE_TTRPG_URL;
-
+export const TTRPG_URL = "https://api.tabletop-almanac.com/api/v1/e5/statblock/search/"
